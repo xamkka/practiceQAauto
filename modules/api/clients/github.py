59c93dafb68fd2905e553264a6b4c14c4b9fb565
Khrystyna Fedoryshyn <xfedoryshyn@gmail.com>
2025-05-10 23:17:26 +0300

    New tests added

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -2,9 +2,27 @@ import requests
 
 
 class GitHub:
-
     def get_user(self, username):
         r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
 
+        return body
+    
+    def search_repo(self, name):
+        r = requests.get(
+            'Https://api.github.com/search/repositories',
+            params={'q': name}
+        )
+        body = r.json()
+        return body
+    
+
+    def get_emoji(self):
+        r = requests.get('https://api.github.com/emojis')
+        body = r.json()
+        return body
+    
+    def get_commits(self, owner, repo):
+        r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+        body = r.json()
         return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -1,5 +1,4 @@
 import pytest
-from modules.api.clients.github import GitHub
 
 
 @pytest.mark.api
@@ -14,5 +13,39 @@ def test_user_non_exists(github_api):
     assert r['message'] == 'Not Found'
 
     
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('practiceQAauto')
+    ##print(r)
+    assert r['total_count'] == 2
+    assert 'practiceQAauto' in r['items'][1]['name']
+
+@pytest.mark.api
+def test_repo_not_found(github_api):
+    r = github_api.search_repo('practiceQAauto_repo_not_found')
+    assert r['total_count'] == 0
+    
 
 
+@pytest.mark.api
+def test_repo_with_single_char_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
+   
+
+@pytest.mark.api
+def test_emoji_present(github_api):
+    r = github_api.get_emoji()
+    assert 'ambulance' in r
+
+
+@pytest.mark.api
+def test_emoji_not_present(github_api):
+    r = github_api.get_emoji()
+    assert 'ambulance1' not in r
+
+
+@pytest.mark.api
+def test_commits(github_api):
+    r = github_api.get_commits('defunkt', 'github-gem')
+    print(r)
